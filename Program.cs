afcc3f0c34262a603947d376769e496918349e4b
Eterance-PC <eterance@sina.com>
2023-03-08 13:57:27 +0800

    修改“按任意键退出”；修改一些文字描述

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace 番剧集数重命名
             {
                 Console.Write("请输入文件夹路径: ");
                 string path = Console.ReadLine();
-                Console.Write("请输入默认季（如S01）。如果路径中的文件夹名没有包含季，将以这个作为默认。回车默认S01 ");
+                Console.Write("请输入默认季（如S01）。如果路径中的文件夹名没有包含季，将以这个作为默认。回车默认S01：");
                 string season_default = Console.ReadLine();
                 if (season_default == null || season_default == "")
                 {
@@ -32,11 +32,11 @@ namespace 番剧集数重命名
                 }
                 if (newNames.Count == 0)
                 {
-                    Console.Write("没有可替换的。");
+                    Console.WriteLine("没有可替换的。");
                 }
                 else 
                 {
-                    Console.Write($"共计 {newNames.Count} 个。输入任意字符取消替换，回车开始替换：");
+                    Console.WriteLine($"共计 {newNames.Count} 个。输入任意字符取消替换，回车开始替换：");
                     string command = Console.ReadLine();
                     if (command == "")
                     {
@@ -48,19 +48,19 @@ namespace 番剧集数重命名
                             }
                             catch (Exception ex)
                             {
-                                Console.Write($"替换因错误被中止。{ex}");
+                                Console.WriteLine($"替换因错误被中止。{ex}");
                             }
                         }
-                        Console.Write($"替换完成。");
+                        Console.WriteLine($"替换完成。");
                     }
                     else 
                     {
-                        Console.Write("替换被取消。");
+                        Console.WriteLine("替换被取消。");
                     }
                 }
-                Console.Write("输入任意字符退出，回车再来一次：");
-                string command2 = Console.ReadLine();
-                if (command2 != null && command2 != "")
+                Console.WriteLine("输入任意字符退出，回车再来一次：");
+                string command2 = Console.ReadKey().Key.ToString();
+                if (command2 != "Enter")
                 {
                     break;
                 }
